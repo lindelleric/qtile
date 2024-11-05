f41279c7aa3be8fb0ddb251e33e164d6b4f85cd9
Eric Lindell <eric.lindell@avensia.com>
2024-11-05 13:12:47 +0100

    init

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash 
+
+# festival --tts $HOME/.config/qtile/welcome_msg &
+# lxsession &
+picom &
+autorandr --change &
+nitrogen --restore &
+volumeicon &
+nm-applet &
+blueman-applet &
